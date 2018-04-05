9e4f580b8925de8301e087c62fc22872c0fe1d47
Guillaume HUSTA <guillaume.husta@gmail.com>
2018-04-05 10:11:43 +0200

    Add log libraries + modify dockerHost for 'travis-ci' profile

--- src/test/java/fr/husta/test/UseDockerContainerIT.java
+++ src/test/java/fr/husta/test/UseDockerContainerIT.java
@@ -2,6 +2,8 @@ package fr.husta.test;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -13,6 +15,8 @@ import static org.assertj.core.api.Assertions.assertThat;
  * Integration Test using a Docker container.
  */
 public class UseDockerContainerIT {
+
+    private static final Logger log = LoggerFactory.getLogger(UseDockerContainerIT.class);
 
     private static String tomcatContainerId;
     private static String tomcatContainerIp;
@@ -26,16 +30,17 @@ public class UseDockerContainerIT {
         tomcatContainerPort = Integer.parseInt(System.getProperty("tomcat.container.port", "-1"));
         dockerMachineIp = System.getProperty("docker-machine.ip");
 
-        System.out.println("tomcatContainerId : " + tomcatContainerId);
-        System.out.println("tomcatContainerIp : " + tomcatContainerIp);
-        System.out.println("tomcatContainerPort : " + tomcatContainerPort);
-        System.out.println("dockerMachineIp : " + dockerMachineIp);
+        log.info("tomcatContainerId : " + tomcatContainerId);
+        log.info("tomcatContainerIp : " + tomcatContainerIp);
+        log.info("tomcatContainerPort : " + tomcatContainerPort);
+        log.info("dockerMachineIp : " + dockerMachineIp);
     }
 
     @Test
     public void checkSocket() throws IOException {
         String localContainerIp = (dockerMachineIp.isEmpty() ? tomcatContainerIp : dockerMachineIp);
         URL url = new URL("http://" + localContainerIp + ":" + tomcatContainerPort);
+        log.info("URL to test : " + url);
         HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
         assertThat(httpURLConnection.getResponseCode()).isEqualTo(200);
     }
