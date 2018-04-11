7610c4bedcdd9acc4594d28fa45cea2e3a913355
Guillaume HUSTA <guillaume.husta@gmail.com>
2018-04-11 12:09:33 +0200

    Add mocks to a unit test (with Mockito)

--- src/test/java/fr/husta/test/SimpleTest.java
+++ src/test/java/fr/husta/test/SimpleTest.java
@@ -1,16 +1,30 @@
 package fr.husta.test;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.io.Serializable;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(MockitoJUnitRunner.class)
 public class SimpleTest {
+
+    @Mock
+    private Serializable myMockData;
 
     @Test
     public void shouldTestNothingInteresting() {
         // just a unit test for the example
 
         assertThat(1 + 2).isEqualTo(3).as("Simple operation test");
+    }
+
+    @Test
+    public void shouldUseMocks() {
+        assertThat(myMockData).isNotNull().as("myMockData should have been mocked");
     }
 
 }
